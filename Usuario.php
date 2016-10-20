fcc941b1d8da25dbfb0cf44bdc16ba7fd0580403
MicaelaConenna <maiten.mica.c@gmail.com>
2016-10-20 11:50:39 -0300

    Add files via upload (#1)

---
+++ Usuario.php
@@ -0,0 +1,39 @@
+<?php
+class Usuario{
+
+private $id;
+private $nombre;
+private $email;
+private $clave;
+private $latitud;
+private $longitud;
+private $es_admin;
+private $esta_bloqueado;
+
+public function __construct($id){
+	
+}
+
+enviar_solicitud_seguimiento($id){}
+
+cambiarEstadoSeguimiento($id_seguidor, $accion){
+	
+}
+agregarPlaylist(){}
+agregarPlaylistFavoritas(){}
+votarPlayList(){}
+reportarPlaylist(){}
+reportarUsuario(){}
+subirCancion(){}
+editarDatosPersonales(){}
+
+//metodos admin
+bloqueaUsuario(){}
+bloqueaPlayList(){}
+generaReporte(){}
+
+}
+
+
+
+?>
